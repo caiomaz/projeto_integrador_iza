715d7b58c83aea4cfe27a115beac8a8691b79227
caiomaz <caiotmzferreira@gmail.com>
2024-10-31 16:17:45 -0300

    adicionar primeira página da aplicação

--- app/views.py
+++ app/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from django.views import View
+from .models import (
+    Categoria, Marca, Produto, Cliente, Venda, ItemVenda, Pagamento,
+    EnderecoEntrega, Avaliacao, Comentario, Cupom, Carrinho, 
+    ItemCarrinho, Desejo, ItemDesejo, Notificacao, Log
+)
 
 
+###############################################################################
+# CONTROLADORES DA APLICAÇÃO (RESPONSÁVEIS POR GERENCIAR AS REQUISIÇÕES HTTP) #
+###############################################################################
 
 
-
-
+class IndexView(View):
+    def get(self, request):
+        return render(request, 'pages/index.html')
+    
+    def post(self, request):
+        pass
+    
